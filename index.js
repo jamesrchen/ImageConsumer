476131012f9a3ae71ba689b89ece2144473f9bda
unknownguy2002 <unknownguy2002>
2019-06-29 05:31:23 +0800

    Added image fetching

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
+const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fs = require("fs")
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-client.login('');
+client.on('message', async msg => {
+    if(msg.attachments.array().length != 0){
+        for (let i = 0; i < msg.attachments.array().length; i++) {
+            fetch(msg.attachments.array()[i].url)
+            .then(res => {
+                const dest = fs.createWriteStream('./images_temp/'+msg.attachments.array()[i].url.split("/")[6]);
+                res.body.pipe(dest);
+            });
+        }
+    }
+});
+
+client.login(process.env.token);
